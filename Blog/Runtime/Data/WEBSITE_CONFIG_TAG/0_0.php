33c2c3ea692e87cb8cd1f566667d01997208b9b0
liuwave <liuwave@qq.com>
2015-06-10 08:11:29 +0800

    删除临时文件

--- Blog/Runtime/Data/WEBSITE_CONFIG_TAG/0_0.php
+++
@@ -1,1 +0,0 @@
-a:9:{s:12:"DEFAULT_LANG";s:5:"zh-cn";s:8:"blogName";s:24:"白袜儿的个人主页";s:8:"subtitle";s:24:"白袜儿的个人主页";s:11:"description";s:30:"这是白袜儿的个人主页";s:4:"copy";s:51:"© 2014 www.baiwar.com All Rights Reserved.Theme by";s:8:"keywords";s:9:"白娃儿";s:12:"templateName";s:7:"Default";s:8:"PAGESIZE";s:1:"8";s:12:"LISTPAGESIZE";s:2:"20";}
