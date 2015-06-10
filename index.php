c37a8fe63ae1e3308232e7f49f47887c2c013b5b
liuwave <liuwave@qq.com>
2015-06-10 11:26:03 +0800

    重新定义template_path 避免冲突

--- index.php
+++ index.php
@@ -3,6 +3,7 @@ if(version_compare(PHP_VERSION,'5.3.0','<'))  die('require PHP > 5.3.0 !');
 define('APP_DEBUG',true);
 define('APP_PATH','./Blog/');
 define("LANG_PATH", "./Blog/Lang/");
+define("TEMPLATE_PATH","./Template/");
 define('DIR_SECURE_FILENAME', 'index.html');
 define('DIR_SECURE_CONTENT', 'No direct script access allowed!');
 require './Core/ThinkPHP.php';
